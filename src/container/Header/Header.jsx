1fb5b48b3b4b9e0d3220a3ed5e780911f065a34e
juliocesaralvesmoreira <juliocesaralvesmoreirajcam@gmail.com>
2023-01-04 17:08:31 -0300

    header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import SubHeading from '../../components/SubHeading/SubHeading';
+
+import { images } from '../../constants'
 
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id="home">
+   <div className='app__wrapper_info'>
+    <SubHeading title="Experimente novos sabores"/>
+    <h1 className='app__header-h1'>O restaurante certo pra a sua dieta</h1>
+   </div>
+   <div className='app__wrapper_img'>
+
+   </div>
   </div>
 );
 
