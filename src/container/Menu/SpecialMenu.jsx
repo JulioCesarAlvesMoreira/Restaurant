04d452b501d0c26c2aa06ef7bf05ff14a77c2576
juliocesaralvesmoreira <juliocesaralvesmoreirajcam@gmail.com>
2023-01-06 19:55:06 -0300

    special menu

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
+import { SubHeading} from '../../components';
+import { images, data } from '../../constants';
 
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
-  </div>
+  <div className='app__specialMenu flex__center section__padding' id="menu">
+    <div className='app__specialMenu-title'>
+      <SubHeading title="Menu para o seu paladar" />
+      <h1 className='headtext__cormorant'>Hoje é um dia Especial</h1>
+    </div>
+    <div className='app__specialMenu-menu'>
+      <div className='app__specialMenu-menu_wine flex__center'>
+        <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
+        <div className='app__specialMenu_menu_items'>
+          {data.wines.map((wine, index) => (
+            <p>{wine.title}</p>
+          ))}
+        </div>
+        </div>
+        <div className='app__specialMenu-menu_img'>
+          <img src={images.menu} alt="menu img" />
+        </div>
+
+        <div className='app__specialMenu-menu_cocktails flex__center'>
+        <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
+        <div className='app__specialMenu_menu_items'>
+          {data.cocktails.map((cocktail, index) => (
+            <p>{cocktail.title}</p>
+          ))}
+        </div>
+        </div>
+      </div>
+      <div style={{margimTpo: '15px'}}>
+        <button type='button' className='custom__button'>Ver Mais</button>
+      </div>
+    </div>
 );
 
 export default SpecialMenu;
